67eb8c622558493478bccbc7fbe1f54cca014f4e
jinimcoroneo <jinim1983@gmail.com>
2017-06-06 14:36:45 +0100

    refactoring the docking station spec file again

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,9 @@ require "docking_station.rb"
 
 describe DockingStation do
   let(:docking_station) { DockingStation.new}
+
   it "responds to release_bike" do
     expect(docking_station).to respond_to :release_bike
   end
+  
 end
