b0bd1c8503cb8d46d21c66658c991c5a3c3b5cdc
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-06 15:13:28 +0100

    Add test that gets a bike and expects bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require "docking_station.rb"
+require "bike.rb"
 
 describe DockingStation do
   let(:docking_station) { DockingStation.new}
@@ -6,5 +7,10 @@ describe DockingStation do
   it "responds to release_bike" do
     expect(docking_station).to respond_to :release_bike
   end
-  
+
+  it "gets a bike and expects it to be working" do
+    bike = docking_station.release_bike
+    expect(bike).to be_working 
+  end
+
 end
