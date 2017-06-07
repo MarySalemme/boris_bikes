81bfe4f0ff91f8aaa66e54bb634d47846aa69648
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-07 17:27:19 +0100

    Added private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,12 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bikes available" if @bikes.empty?
+    raise "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    raise "Docking station is full" if @bikes.count >= 20
+    raise "Docking station is full" if full?
     @bikes << bike
   end
+
+private 
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
 end
